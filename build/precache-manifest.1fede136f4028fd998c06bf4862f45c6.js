52204fed9348c0c2c7ae1aaa6a2f1c7cc70aa09c
inntechart <inntechart@gmail.com>
2020-05-28 18:53:14 +0400

    build prod

---
+++ build/precache-manifest.1fede136f4028fd998c06bf4862f45c6.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "979e837c669698263fe5ce97d5a939f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "848981d24ec839288508",
+    "url": "/static/css/main.40c45fb9.chunk.css"
+  },
+  {
+    "revision": "7ddf4904cc7af125a49b",
+    "url": "/static/js/2.432503b7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.432503b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "848981d24ec839288508",
+    "url": "/static/js/main.ca72838f.chunk.js"
+  },
+  {
+    "revision": "208943f1bc2552b4a785",
+    "url": "/static/js/runtime-main.59bb8a28.js"
+  },
+  {
+    "revision": "1c3cd0ddd208c7ccd29199241ddc9364",
+    "url": "/static/media/1.1c3cd0dd.png"
+  },
+  {
+    "revision": "ad54553b83da9bd0d1f1bf1f748cd739",
+    "url": "/static/media/10.ad54553b.png"
+  },
+  {
+    "revision": "522284a12db455c3da875ce43639583f",
+    "url": "/static/media/2.522284a1.png"
+  },
+  {
+    "revision": "e691f6a5dcbed07dfd0385414107bf2b",
+    "url": "/static/media/3.e691f6a5.png"
+  },
+  {
+    "revision": "ae886935c6fb0d4dede1de785837b86e",
+    "url": "/static/media/4.ae886935.png"
+  },
+  {
+    "revision": "5944a4e4e02b041fe49ac6e1ab66b0a9",
+    "url": "/static/media/5.5944a4e4.png"
+  },
+  {
+    "revision": "f50d009fc29638cf44023b496f8b426c",
+    "url": "/static/media/6.f50d009f.png"
+  },
+  {
+    "revision": "69150c231c0193b9bc5fa66377d9ca12",
+    "url": "/static/media/7.69150c23.png"
+  },
+  {
+    "revision": "3aa3761d3dc018fcafb6373024ded17c",
+    "url": "/static/media/8.3aa3761d.png"
+  },
+  {
+    "revision": "0167677e8c6c9b3a4fd50d7ee6d1c521",
+    "url": "/static/media/9.0167677e.png"
+  },
+  {
+    "revision": "eac0b2ed686b219e45c3aa4a1b6cc47a",
+    "url": "/static/media/ann.eac0b2ed.png"
+  },
+  {
+    "revision": "7ddd84771911695db17827e7bcec5cb7",
+    "url": "/static/media/bank.7ddd8477.png"
+  },
+  {
+    "revision": "09aab2c38f4ea88a5f9e5b2ac5f995c1",
+    "url": "/static/media/bg2.09aab2c3.png"
+  },
+  {
+    "revision": "f8182bd50dccc2cb9c1b8f20ca3825a6",
+    "url": "/static/media/calendar.f8182bd5.png"
+  },
+  {
+    "revision": "0a60749a5e6ec1084a48fcd751d5ac6b",
+    "url": "/static/media/child.0a60749a.png"
+  },
+  {
+    "revision": "c3618cae0dbbaed9196bc61a09472636",
+    "url": "/static/media/close.c3618cae.svg"
+  },
+  {
+    "revision": "02d4bdf83623ab5b3bb7282d381ae143",
+    "url": "/static/media/comp.02d4bdf8.png"
+  },
+  {
+    "revision": "51b7e94881e3071c7484b90acb65b86b",
+    "url": "/static/media/logo.51b7e948.svg"
+  },
+  {
+    "revision": "dd6e1d31b60ce71df82359b61cebb49b",
+    "url": "/static/media/payments.dd6e1d31.png"
+  },
+  {
+    "revision": "8a086f33daeda01e2c2f533fc33b6820",
+    "url": "/static/media/search.8a086f33.svg"
+  },
+  {
+    "revision": "3f28d1101d740af507c5ec41ca4b94e1",
+    "url": "/static/media/section_bg.3f28d110.png"
+  },
+  {
+    "revision": "f8c88985184f7fc565fd7b460083ea1d",
+    "url": "/static/media/tArrow.f8c88985.svg"
+  },
+  {
+    "revision": "38024460d57681e851eb8dfe9a981313",
+    "url": "/static/media/teach.38024460.png"
+  },
+  {
+    "revision": "5d43790b81bd9fd0a16676a99a12745a",
+    "url": "/static/media/teach2.5d43790b.png"
+  }
+]);
